e682f47dbc732ea12fd6a51c32124c6e87f14cc3
munyekenye <munyekenyelilian@gmail.com>
2019-12-15 20:06:03 +0300

    Declared variable PizzaFavour, pizzaTopping and PizzaCrust

--- js/Script.js
+++ js/Script.js
@@ -1,5 +1,28 @@
+// // declaring variables
 var pizzaSize =[ 
     "Regular",
     "Medium",
     "Large",
      ];
+     
+var pizzaFlavour = [
+    "Berry",
+    "Butternut Squash",
+    "Chicken",
+    "Hummus",
+    "Turkish Ground"
+]
+var pizzaTopping = [
+    "Onion", 
+    "Mushroom", 
+    "bacon", 
+    "black olives",
+   ];
+
+   var pizzaCrust = [ 
+    "Thin crust",
+    "Thick crust", 
+    "Deep crust", 
+    "Stuffed crust",
+   ];
+   
