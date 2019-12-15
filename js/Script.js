62ce0b88990aa1aec45f063d29d37529321ca1bd
munyekenye <munyekenyelilian@gmail.com>
2019-12-15 21:51:08 +0300

    Added calling function

--- js/Script.js
+++ js/Script.js
@@ -25,4 +25,87 @@ var pizzaTopping = [
     "Deep crust", 
     "Stuffed crust",
    ];
-   
+   // calling functions
+function getFlavour() {
+
+    var flavour = document.getElementById("pizzaflavour").value;
+    return parseInt(flavour);
+  }
+  function getSize() {
+    var size = document.getElementById("pizzasize").value;
+    return parseInt(size);
+  }
+  
+  function getCrust() {
+  
+  
+    var crust = document.getElementById("pizzacrust").value;
+  
+  
+    return parseInt(crust);
+  
+  
+  }
+  
+  
+  
+  
+  
+  function getTopping() {
+  
+  
+    var topping = document.getElementById("pizzatopping").value;
+  
+  
+    return parseInt(topping);
+  
+  
+  }
+  
+  
+  
+  
+  
+  function getNumber() {
+  
+  
+    var number = document.getElementById("number").value;
+  
+  
+    return parseInt(number);
+  
+  
+  }
+  
+  
+  
+  
+  
+  function getAmount() {
+  
+  
+    var results =(getFlavour() + getSize() + getCrust() + getTopping()) * getNumber();
+  
+  
+    alert(
+  
+  
+      "You've ordered " +
+  
+  
+      getNumber("") +  
+  
+  
+      " pizza's," +    
+  
+  
+      "which amounts to Ksh. " +
+  
+  
+      results +
+  
+  
+      " Thanks for your order,welcome again!"
+  
+  
+    );
